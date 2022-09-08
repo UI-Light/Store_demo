82af709f62564f54ab99ea223c61c482047711c4
Aonani <Lightkarlton@gmail.com>
2022-09-08 00:12:22 -0700

    Animate splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        
           primaryColor: const Color(0xffFFA451),
           fontFamily: 'BrandonGrotesque'),
       home: const SplashScreen(),
--- lib/ui/splash_screen.dart
+++ lib/ui/splash_screen.dart
@@ -10,79 +10,175 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation vector1Animation;
+  late Animation vector2Animation;
+  late Animation vector3Animation;
+  late Animation vector4Animation;
+  late Animation vector5Animation;
+  late Animation rectangleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    );
+
+    vector1Animation = Tween<double>(begin: 0.0, end: 300.0).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: const Interval(
+          0.0,
+          0.2,
+          curve: Curves.easeInOut,
+        ),
+      ),
+    );
+
+    vector2Animation = Tween<double>(begin: -400.0, end: 50.0).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: const Interval(
+          0.2,
+          0.3,
+          curve: Curves.easeInOut,
+        ),
+      ),
+    );
+
+    vector3Animation = Tween<double>(begin: -400.0, end: 60.0).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: const Interval(
+          0.2,
+          0.3,
+          curve: Curves.easeInOut,
+        ),
+      ),
+    );
+
+    vector4Animation = Tween<double>(begin: -150, end: 180.0).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: const Interval(
+          0.3,
+          0.4,
+          curve: Curves.easeInOut,
+        ),
+      ),
+    );
+
+    vector5Animation = Tween<double>(begin: -150.0, end: 180.0).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: const Interval(
+          0.3,
+          0.4,
+          curve: Curves.easeInOut,
+        ),
+      ),
+    );
+
+    rectangleAnimation = Tween<double>(begin: -100.0, end: 260.0).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: const Interval(
+          0.3,
+          0.4,
+          curve: Curves.easeInOut,
+        ),
+      ),
+    );
+
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // return AnimatedSplashScreen(
-    //   splash: Column(
-    //     children: [
-    //       Image.asset('assets/images/Vector-1.png'),
-    //     ],
-    //   ),
-    //   nextScreen: const WelcomeScreen(),
-    //   splashTransition: SplashTransition.slideTransition,
-    //   duration: 10000,
-    // );
     return Scaffold(
       body: Center(
-        child: Stack(
-          children: <Widget>[
-            Positioned(
-              top: 300,
-              left: 40,
-              right: 40,
-              //duration: const Duration(seconds: 7),
-              //top: 100,
-              child: Image.asset('assets/images/Vector-1.png'),
-            ),
-            Positioned(
-              top: 240,
-              left: 110,
-              right: 50,
-              //duration: const Duration(seconds: 2),
-              child: Image.asset('assets/images/Vector-2.png'),
-            ),
-            Positioned(
-              top: 240,
-              left: 60,
-              right: 120,
-              // duration: const Duration(seconds: 2),
-              child: Image.asset('assets/images/Vector-3.png'),
-            ),
-            Positioned(
-              top: 180,
-              left: 50,
-              right: 25,
-              //duration: const Duration(seconds: 2),
-              child: Image.asset('assets/images/Vector-4.png'),
-            ),
-            Positioned(
-              top: 180,
-              left: 40,
-              right: 85,
-              //duration: const Duration(seconds: 2),
-              child: Image.asset('assets/images/Vector-5.png'),
-            ),
-            Positioned(
-              bottom: 240,
-              left: 40,
-              right: 40,
-              //duration: const Duration(seconds: 2),
-              child: Image.asset('assets/images/Rectangle-47.png'),
-            ),
-            Positioned(
-              bottom: 260,
-              left: 130,
-              child: Text(
-                'Fruit Hub',
-                style: GoogleFonts.badScript(
-                  fontSize: 24,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w400,
-                ),
+        child: AnimatedBuilder(
+          animation: controller,
+          builder: (context, child) => Stack(
+            children: <Widget>[
+              Positioned(
+                top: vector1Animation.value,
+                left: 40.0,
+                right: 40.0,
+                child: Image.asset('assets/images/Vector-1.png'),
               ),
-            ),
-          ],
+              Positioned(
+                top: 240.0,
+                left: 110,
+                right: vector2Animation.value,
+                child: Image.asset('assets/images/Vector-2.png'),
+              ),
+              Positioned(
+                top: 240.0,
+                left: vector3Animation.value,
+                right: 120, //vector3Animation.value,
+
+                child: Image.asset('assets/images/Vector-3.png'),
+              ),
+              Positioned(
+                top: vector4Animation.value,
+                left: 50.0,
+                right: 25.0,
+                child: Image.asset('assets/images/Vector-4.png'),
+              ),
+              Positioned(
+                top: vector5Animation.value, //180.0,
+                left: 40.0,
+                right: 85.0,
+                child: Image.asset('assets/images/Vector-5.png'),
+              ),
+              Positioned(
+                bottom: rectangleAnimation.value, //260.0,
+                left: 70.0,
+                right: 70.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20.0),
+                        bottomRight: Radius.circular(20.0),
+                      )),
+                  child: Center(
+                    child: Text(
+                      'Fruit Hub',
+                      style: GoogleFonts.badScript(
+                        fontSize: 24,
+                        color: Theme.of(context).colorScheme.onPrimary,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ), //Image.asset('assets/images/Rectangle-47.png')
+              ),
+              // Positioned(
+              //   bottom: 260.0,
+              //   left: 130.0,
+              //   child: Text(
+              //     'Fruit Hub',
+              //     style: GoogleFonts.badScript(
+              //       fontSize: 24,
+              //       color: Colors.black,
+              //       fontWeight: FontWeight.w400,
+              //     ),
+              //   ),
+              // ),
+            ],
+          ),
         ),
       ),
     );
